0c695a65c09d04e0c16a899c06186d4eba7b2f38
johnpatbatt <dev.john.batt@gmail.com>
2022-01-05 02:28:31 -0500

    Fix for the deprecation issue for mongoDB

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ const connect = mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: false,
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
