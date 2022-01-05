b7b8f61c141249ad61d6aac211ad48786a2f04be
johnpatbatt <dev.john.batt@gmail.com>
2022-01-05 02:34:53 -0500

    Update

--- server/index.js
+++ server/index.js
@@ -27,7 +27,11 @@ const connect = mongoose
 
 app.use(cors())
 
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
 
